02a0d9a4c519e2d91bf1b5a0c2027a0e51b9cb91
azaelrock0 <azaelrock0@gmail.com>
2021-11-19 19:13:59 -0500

    Coneccion con base de datos
    Coneccion con base de datos

---
+++ routes/product.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+const Product = require('../models/product')
+
+// POST request - crear un nuevo producto
+router.post('/products', async (req, res) =>{
+    try {
+        let product = new Product()
+        product.title = req.body.title
+        product.description = req.body.description
+        product.photo = req.body.photo
+        product.stockQuantity = req.body.stockQuantity
+        
+        await product.save()
+
+        res.json({
+            status: true,
+            message: "Successfully saved"
+        })
+    }catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message
+        })
+    }
+})
+
+// GET request - recibir todos los productos
+
+// GET request - recibir un solo producto
+
+// PUT request - Actualizar un producto
+
+// DELETE request - eliminar un producto
+
+module.exports = router
