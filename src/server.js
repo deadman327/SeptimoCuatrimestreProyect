544297bd992f62e80f18a35fb7571c00bb28a09a
DEITRII <juandeitri@gmail.com>
2021-12-01 03:37:46 -0500

    Paginas y Responsive

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 
 
 // initilizitations
@@ -26,8 +27,9 @@ app.engine('.hbs', exphbs.engine ({
 app.set('view engine', '.hbs');
 
 // Middlewares
-app.use(express.urlencoded({extended: false}));
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
 
 
 // Global Variables
