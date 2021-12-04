26f46fadbd81d04234f87cee6aedf11f256547f2
deadman327 <59692563+deadman327@users.noreply.github.com>
2021-12-04 02:52:28 -0600

    Correciones Login - part 1

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
-
 const passport = require('passport');  
 
 
